190c984862e3014abdc6e61666aa423da0da157b
lalitm1004 <lalitm1004@gmail.com>
2025-06-16 21:51:53 +0530

    feat(frontend): create .env.example + change jwt token name

--- frontend/src/lib/stores/JwtTokenStore.ts
+++ frontend/src/lib/stores/JwtTokenStore.ts
@@ -1,7 +1,7 @@
 import createCookiePersistentStore from "$lib/utils/store/createCookiePersistentStore";
 import { stringCodec } from "$lib/utils/store/storeCodecs";
 
-export const JWT_TOKEN_NAME = 'jwt-token';
+export const JWT_TOKEN_NAME = 'gdsc-member-portal-jwt-token';
 const {
     store: JwtTokenStore, set: setJwtToken
 } = createCookiePersistentStore<string>({
