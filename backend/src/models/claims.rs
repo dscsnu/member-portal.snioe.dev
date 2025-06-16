47163d7e68a0d1fda9df8013bedb6407ed998d70
lalitm1004 <lalitm1004@gmail.com>
2025-06-17 00:53:39 +0530

    feat(models): create claims model

---
+++ backend/src/models/claims.rs
@@ -0,0 +1,29 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Claims {
+    #[serde(rename = "sub")]
+    pub user_id: String,
+
+    pub app_metadata: AppMetadata,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct AppMetadata {
+    pub custom_claims: CustomClaims,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct CustomClaims {
+    pub groups: Vec<Group>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Group {
+    pub id: String,
+    pub name: String,
+    pub permissions: Vec<Permission>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Permission(String);
